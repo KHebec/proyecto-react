662874e8ac476c6039eed3daa6f08b19fb77b130
jorge niglia <jorge.niglia@gmail.com>
2022-08-14 14:33:20 -0300

    carousel v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Gallery from "./components/gallery/Gallery";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Gallery />
+    </div>
+  );
 }
 
 export default App;
