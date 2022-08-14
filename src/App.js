e8a620d341837f5ddf150559f98781bbe115ce02
jorge niglia <jorge.niglia@gmail.com>
2022-08-14 18:06:03 -0300

    logo svg centrado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Gallery from "./components/gallery/Gallery";
+import logo from "./assets/images/logo.svg";
 
 function App() {
   return (
     <div className="App">
+      <img className="logo" src={logo} alt="logo" />
       <Gallery />
     </div>
   );
