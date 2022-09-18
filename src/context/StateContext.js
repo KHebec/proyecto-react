c3fef085a8673ecf45eb37d60fd02a2dd8a30df0
jorge niglia <jorge.niglia@gmail.com>
2022-09-18 11:02:57 -0300

    https3

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -8,9 +8,9 @@ export const StateContext = createContext();
 
 const StateProvider = (props) => {
   const [state, dispatch] = useReducer(stateReducer, stateInitialState);
-  const productsURL = "http://localhost:5000/products",
-    cartURL = "http://localhost:5000/cart";
-  const authorsURL = "http://localhost:5000/authors";
+  const productsURL = "https://localhost:5000/products",
+    cartURL = "https://localhost:5000/cart";
+  const authorsURL = "https://localhost:5000/authors";
   const { products, cart, authors } = state;
   const [loading, setLoading] = useState(false);
 
