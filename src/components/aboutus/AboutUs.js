ab0770ad77a2c4c446cba4f6f40afc7a6a404c47
jorge niglia <jorge.niglia@gmail.com>
2022-09-18 17:07:04 -0300

    about us

--- src/components/aboutus/AboutUs.js
+++ src/components/aboutus/AboutUs.js
@@ -12,6 +12,9 @@ const AboutUs = () => {
       <h2 className="mb-12 text-xl font-bold text-center text-spirits">
         Sobre los autores del proyecto en React
       </h2>
+      <h3 className="mb-12 text-lg font-semibold text-center text-spirits">
+        Grupo de proyectos 3 de S0322 de escuela Numen{" "}
+      </h3>
       <section className="flex flex-wrap justify-center gap-6 mb-10 xl:gap-8">
         {authors.map((author) => (
           <CardAuthor key={author.id} author={author} />
